430f0499381c1c643c9b9c51374813acd5423eff
Alex Ellis <alexellis2@gmail.com>
2016-01-18 23:48:49 +0000

    redis

--- images/expressredis4.x/app.js
+++ images/expressredis4.x/app.js
@@ -2,8 +2,10 @@ var express = require ('express');
 var app = express();
 var redis = require('redis');
 
-app.get('/', function(req,res){
-    var client = redis.createClient(process.env.REDIS_PORT_6379_TCP_PORT, process.env.REDIS_PORT_6379_TCP});
+app.get('/', function(req,res) {
+    var client = redis.createClient(process.env.REDIS_PORT_6379_TCP_PORT, 
+				    process.env.REDIS_PORT_6379_TCP_ADDR);
+
     client.incr("hit_count", function() {
         client.get("hit_count", function(err, data) {
             res.json({"message" : "Ping", "count": data});
