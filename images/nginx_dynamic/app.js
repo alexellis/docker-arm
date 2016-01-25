613829ff0b84652012b9bc74e41927c41c085ef2
Alex Ellis <alexellis2@gmail.com>
2016-01-25 21:28:54 +0000

    Clean up comments

--- images/nginx_dynamic/app.js
+++ images/nginx_dynamic/app.js
@@ -1,4 +1,6 @@
 var request = require('request');
+
+// Query the Docker Remote API directly.
 var ops = {
  uri: "http://192.168.0.200:4000/containers/json?all=0&size=1",
  method: "GET"
@@ -11,15 +13,9 @@ request(ops, function(err, res, body) {
     if(err) {
         return console.error(err);
     }
+    // Print to stdout so we can pipe into nginx.conf
     var containers = JSON.parse(body);
-//   console.log(containers);
     containers.forEach(function(host) {
-/*        console.log(host.Ports);
-	console.log(host.Names);
-	console.log("");
-*/
         console.log("\tserver "+host.Ports[0].IP + ":" + host.Ports[0].PublicPort+ ";");
-       
     });
-
 });
