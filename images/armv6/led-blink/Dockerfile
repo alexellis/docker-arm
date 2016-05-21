ccf207b75f26a90af2d8071a417eedce77e97b1b
Alex Ellis <alexellis2@gmail.com>
2016-05-21 20:55:54 +0000

    Adding led-blink example.
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ images/armv6/led-blink/Dockerfile
@@ -0,0 +1,5 @@
+FROM resin/rpi-raspbian
+
+ADD ./start.sh ./
+
+CMD ["./start.sh"]
---
+++ images/armv6/led-blink/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker build -t alexellis2/led-blink:armv6 .
+
+echo "alexellis2/led-blink:armv6 built"
