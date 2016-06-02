98d9f5e44384dad1a114b3d87eb596cb1ee0d463
Alex Ellis <alexellis2@gmail.com>
2016-06-02 21:48:10 +0000

    Static nginx

---
+++ images/armv6/nginx-arm/app.js
@@ -0,0 +1,24 @@
+var request = require('request');
+
+var swarmUri = process.env.SWARM_URI;
+var labelFilter = process.env.LABEL_FILTER;
+
+// Query the Docker Remote API directly.
+var ops = {
+ uri: "http://"+swarmUri+"/containers/json?all=0&size=1",
+ method: "GET"
+};
+
+var filter = {"label": [labelFilter]};
+ops.uri = ops.uri +"&filters="+ JSON.stringify(filter);
+
+request(ops, function(err, res, body) {
+    if(err) {
+        return console.error(err);
+    }
+    // Print to stdout so we can pipe into nginx.conf
+    var containers = JSON.parse(body);
+    containers.forEach(function(host) {
+        console.log("\tserver "+host.Ports[0].IP + ":" + host.Ports[0].PublicPort+ ";");
+    });
+});
