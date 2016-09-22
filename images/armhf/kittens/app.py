529e81f195d2d11f6564a0f5a3b195d2e6d0e21e
Alex Ellis <alexellis2@gmail.com>
2016-09-22 21:16:04 +0000

    sample service

--- images/armhf/kittens/app.py
+++ images/armhf/kittens/app.py
@@ -1,6 +1,7 @@
 import os
 import json
 import socket
+import os.path
 
 from flask import Flask, request, render_template, send_from_directory
 
@@ -8,15 +9,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def sensors():
-    host = socket.getfqdn()
-
     props = {"hostname": host, "host_hostname": host_hostname}
     print(props)
     return render_template("kittens.html", props = props)
 
-if __name__ == '__main__':
-    f = open("/etc/host.hostname")
-    host_hostname = f.read()
-    f.close()
+if( __name__ == '__main__'):
+
+    host = socket.getfqdn()
+    if(os.path.isfile("/etc/host.hostname")):
+        f = open("/etc/host.hostname")
+        host_hostname = f.read()
+        f.close()
+    else:
+       host_hostname = host
+
     print("0.0.0.0")
     app.run(debug=True, host='0.0.0.0')
--- images/armhf/kittens/sample_service.sh
+++ images/armhf/kittens/sample_service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker service rm kitties
+
 docker service create --mount type=bind,source=/etc/hostname,target=/etc/host.hostname \
  --name kitties --replicas=4 --network armnet \
  --publish 5000:5000 \
